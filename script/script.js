25da3914ea773fdbda6613ea8df2c8ae9aaf5fdb
Tracie Smith <90426413+tsmit655@users.noreply.github.com>
2024-05-14 14:03:27 -0400

    checkpoint

--- script/script.js
+++ script/script.js
@@ -23,38 +23,39 @@ let teams = [
     populateTeamList()
    };
     
-
+   // once the button is clicked 
    function onClickedTeamSelectButton(){
     console.log(teamList.value);
     
     let selectedValue = teamList.value;
-    let TeamSelected = getTeamByCode(teamCode);
-    message = `You selected: ${TeamSelected.name} (Code: ${TeamSelected.code}) who plays in ${TeamSelected.plays}.`;
-    outputTeam.innerHTML = message;
+    let TeamSelected = getTeamByCode(selectedValue);
+    
+    
 
 
+    // adds the outout once a team is selected and the button is clicked
+    if(TeamSelected){
+        let message = `You selected: ${TeamSelected.name} ${TeamSelected.code} who plays in ${TeamSelected.plays}.`;
+        outputTeam.innerHTML = message;
+    }else{
+        alert("No team was selected");
+    }
 
-    if (selectedValue == null) {
-        alert("No state was selected");
-        return; // exit the event handler
-       }
-
-    // addParagraph(selectedCode, selectedName);
+    
    };
  
 
    function populateTeamList(){
     console.log("populate");
-    
-    function getTeamByCode(teamCode) {
-    let t;
-    for(let i = 0; i < teams.length; i++){
-        if(teams[i].code == teamCode){
-            t = teams[i];
-        }
-    }
-    return t;
-}
+
+    // Add default option
+    let defaultOption = document.createElement("option");
+    defaultOption.textContent = "Select a team";
+    defaultOption.value = '';
+    teamList.appendChild(defaultOption);
+
+
+    // Add team option
     for(let i = 0; i < teams.length; i++){
         let newOption = document.createElement("option");
         newOption.textContent = teams[i].name;
@@ -64,3 +65,12 @@ let teams = [
     }
    
    } 
+   // add code
+    function getTeamByCode(teamCode) {
+    for(let i = 0; i < teams.length; i++){
+        if(teams[i].code == teamCode){
+            return teams[i];
+        }
+    }
+    return t;
+}
